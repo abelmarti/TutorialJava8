2cc13909ebd8beae9d3191724513c1b9a4c48ec8
abelmarti <abelmartinez.garcia1991@gmail.com>
2018-07-11 21:59:10 -0400

    proyecto eclipse generado

---
+++ Java8Tutorial/src/main/java/cl/tutorial/java/Java8Tutorial/App.java
@@ -0,0 +1,13 @@
+package cl.tutorial.java.Java8Tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
